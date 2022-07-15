fec68a42941726ea1dab2db02c98663290edf07b
Mariko Iwata <3701668+MarikoIwata@users.noreply.github.com>
2022-07-15 13:50:48 -0400

    added pseudocode and starting to code. fixed browser not identified issue

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ EDITS
 3. the foreground script
 4. the popup page
 5. the options page
+
+# issues I came up with
+- using browser.something. I would get an error "browser is not identified". I used [this thread](https://github.com/mdn/webextensions-examples/issues/194) to use chrome.something instead since chrome implements API's using the chrome. namespace. That said, that is confusing because the MDN documentation uses browser...
--- tabSorter.js
+++ tabSorter.js
@@ -1,11 +1,26 @@
 //variable is html element button that comes up on extension popup
-let sortButton = document.getElementById("sortButton");
+// let sortButton = document.getElementById("sortButton");
 
-sortButton.addEventListener("click", () => {
-    //after clicking the grey button
+// sortButton.addEventListener("click", () => {
+//     //after clicking the button, invoke sort tabs
 
-} )
+// } )
 
+//use chrome instead of browser
+let querying = chrome.tabs.query({})
+console.log(querying)
+
+// function sortTabs(){
+//     //get array of open tabs with title and index
+//     //iterate through array and compare title with title next to it, use something like bubble sort to move tabs to sort alphabetically
+
+//     //use tabs.move() to move the tabs
+//     //tabs.Tab contains information about tab
+
+// }
+
+
+/* ---------------- CODE FROM TUTORIAL TO CHANGE BACKGROUND COLOR ------------------*/
 // chrome.storage.sync.get("color", ({ color }) => {
 //   changeColor.style.backgroundColor = color;
 // });
