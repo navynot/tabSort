9f40b45a02c841a800b2931557e4c49c67875f71
Tony Van <tonyvan@Tonys-MBP.lan>
2022-07-15 19:42:29 -0400

    created array of sorted ids

--- tabSorter.js
+++ tabSorter.js
@@ -1,14 +1,30 @@
 
-let tabData;
+const tabData = {};
+let sortedArr = [];
+let sortedObj = {};
+let sortedId = [];
+
+let sortButton = document.getElementById("sortButton");
 
 //use chrome instead of browser that MDN uses, this gets all info on tabs
 let querying = chrome.tabs.query({currentWindow: true})
-    .then((value) => {
-        //value is array of tabs and their details
-        console.log(value)
-        //run the function sortTabs
-
+    .then(tabs => {
+        sortButton.addEventListener("click", () => {
+            //add titles and ids to tabData object
+            for (let i = 0; i < tabs.length; i++) {
+                tabData[tabs[i].title] = tabs[i].id
+            }
+            //get and sort array of titles and then add the sorted titles with their corresponding ids to sortedObj
+            //can use reduce method here to optimize in the future
+            sortedArr = Object.keys(tabData).sort().forEach(key => {sortedObj[key] = tabData[key]})
+            //create array that has the ids sorted in alphabetical order
+            //will use these sorted ids to with tabs.move
+            sortedId = Object.values(sortedObj)
+            console.log(sortedObj)
+            console.log(sortedId)
+        })
     })
+
 
 // function sortTabs(value){
 //         //get array of open tabs with title and index loop through array 
