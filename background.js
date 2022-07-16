61d590d6d753b18cd259ac1ff272006d08a3a6ed
Tony Van <tonyvan@Tonys-MBP.lan>
2022-07-15 22:47:47 -0400

    fully functioning sort/move alphabetically

--- background.js
+++ background.js
@@ -3,15 +3,3 @@
 //The background script doesn't actually have access to any of the webpages your user is viewing, so your background script can't manipulate the DOM.
 
 //we don't need to save any information for sorting tabs, so I'm commenting this out -Mariko
-
-
-
-
-
-// let color = '#3aa757';
-
-
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-//   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
--- tabSorter.js
+++ tabSorter.js
@@ -19,29 +19,18 @@ let allTabs = chrome.tabs.query({currentWindow: true})
                 acc[cur] = tabData[cur];
                 return acc;
             }, {});
-            console.log(sortedTab);
 
             //create array of values in sorted order
             sortedId = Object.values(sortedTab);
-            console.log(sortedId);
+
+            
+            moveTabs(sortedId);
         })
     });
 
 
-// function sortTabs(value){
-//         //get array of open tabs with title and index loop through array 
-//         //check if title[i] is > title[i+1] if so, then move tab for i right
-//     for(let i = 1; i<value.length;i++){
-//         let titleLeft = value[i].title;
-//         for(let j = 0; j<value.length; i++){
-//             let titleRight = value[j].title;
-//             if(titleLeft > titleRight){
-//                 //move left to right - moving returns a promise
-//                 let moving = chrome.tabs.move(value.tabs[i].id, {index:i+1})
-//                 console.log(moving)
-
-//             }
-//         }
-//     }
-
-// // }
+function moveTabs(ids){
+    ids.forEach(id => {
+        chrome.tabs.move(id, {index: ids.indexOf(id)})
+    })
+}
