f22e23b19a172db3b2e74479214f03894d6b9f01
Tony Van <tonyvan@Tonys-MBP.lan>
2022-07-17 19:21:25 -0400

    updated code for working state changes

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
 
 chrome.storage.onChanged.addListener((changes, area) => {
     if (area === 'sync' && changes.auto?.newValue) {
-        const alphaSort = Boolean(changes.auto.newValue.alpha);
-        const rAlphaSort = Boolean(changes.auto.newValue.rAlpha);
-        const rCurrentSort = Boolean(changes.auto.newValue.rCurrent);
+        Boolean(changes.auto.newValue.enabled);
+
+        Boolean(changes.auto.newValue.alpha);
+        Boolean(changes.auto.newValue.rAlpha);
+        Boolean(changes.auto.newValue.rCurrent);
+
+        Boolean(changes.auto.newValue.alphaDisabled);
+        Boolean(changes.auto.newValue.rAlphaDisabled);
+        Boolean(changes.auto.newValue.rCurrentDisabled);
+
     }
 })
